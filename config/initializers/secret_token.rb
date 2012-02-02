c3d371d179b17c674c1857de4597e35e5a2c33c1
Austin Lee D4L <Austin.L.D4L@gmail.com>
2012-02-01 22:13:51 -0800

    make rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectEuler::Application.config.secret_token = '702c8df767daaa2d1ee17903d6c49a0e3afcba33d15136fedbaf32580ed7435b4926215c092e469324ef04e8f0b86bfe9fb9635a1a6f1aa65f534d73ce8891f2'
