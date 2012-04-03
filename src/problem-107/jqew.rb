df359d9ee9ad0858bd6637f7459523945e48009b
Austin Lee D4L <Austin.L.D4L@gmail.com>
2012-04-03 02:22:50 -0700

    Get closer to a solution for 107

--- src/problem-107/jqew.rb
+++ src/problem-107/jqew.rb
@@ -10,27 +10,74 @@ def jqew
     end
     counter += 1
   end
-  minimal = matrix.minimize
-  minimal.total
+  matrix.minimize
+  p matrix.edges.size
+  matrix.total
 end
 
 class Jqew1 # this represents the entire matrix
-  def initialize
-    @edges = Array.new
+  attr_accessor :edges
+  def initialize *a
+    if a.count == 1
+      @edges = Array.new(a[0].edges)
+    else
+      @edges = Array.new
+    end
   end
   def push i
     @edges.push i
   end
+  def pop
+    @edges.pop
+  end
+  def delete i
+    @edges.delete i
+  end
   def connected?
-    (@edges.collect {|i| i.loc1} + @edges.collect {|i| i.loc2}).uniq == Array.new(40) {|i| i+1}
+    temp = Array.new
+    group = Array.new([@edges.first.loc1])
+    while temp.size != group.size
+      group.each do |i|
+        temp += connected_to_vertex i
+      end
+      group += temp
+      temp.uniq!
+      group.uniq!
+      temp, group = group, temp
+      #bipartite_test = Array.new
+      #group.each do |i|
+      #  bipartite_test += connected_to_vertex i
+      #end
+      #bipartite_test.uniq!
+      #break if temp.size == bipartite_test.size
+    end
+    temp.count == 40
   end
   def total
     @edges.collect{|i| i.value}.inject(&:+)
   end
   def minimize
-    result = Jqew1.new
-    @edges.sort!{|i,k| i.value <=> k.value}
-    result
+    # successively delete the largest element and ensure it's still connected
+    @edges.sort!{|i,k| i.value <=> k.value}.reverse!
+    temp = Jqew1.new(self)
+    @edges.each do |e|
+      tempedge = temp.delete e
+      if not temp.connected?
+        temp.push tempedge
+      end
+    end
+    @edges = temp.edges
+  end
+  def connected_to_vertex i # where i is the point
+    result = Array.new
+    @edges.each do |j|
+      if j.loc1 == i
+        result.push j.loc2
+      elsif j.loc2 == i
+        result.push j.loc1
+      end
+    end
+    result.uniq
   end
 end
 
